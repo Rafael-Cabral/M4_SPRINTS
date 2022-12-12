75907caf31d8b5b3c01daa33b6ca7a5bea06ad35
Rafael-Cabral <99192195+Rafael-Cabral@users.noreply.github.com>
2022-12-12 07:05:41 -0300

    Cripto

--- SEMANA_09/EXERCICIOS/servCripto/servCripto.ino
+++ SEMANA_09/EXERCICIOS/servCripto/servCripto.ino
@@ -1,46 +1,42 @@
-Vídeo: https://github.com/Rafael-Cabral/M4_SPRINTS/tree/main/SEMANA_07
-
-Img: https://github.com/Rafael-Cabral/M4_SPRINTS/tree/main/SEMANA_07
-
 #include <Arduino.h>
 #include <ArduinoJson.h>
 #include <HTTPClient.h>
 #include <WiFi.h>
 #include <stdio.h>
 #include <String>
-const char* SSIDS = “Inteli-COLLEGE”;
-const char* PWD = “QazWsx@123";
+const char* SSIDS = "Inteli-COLLEGE";
+const char* PWD = "QazWsx@123";
 char * mensagem;
 char converter(char ch, int chave) {
   if (!isalpha(ch)) return ch;
-  char offset = isupper(ch) ? ‘A’ : ‘a’;
+  char offset = isupper(ch) ? 'A' : 'a';
   return (char)((((ch + chave) - offset) % 26) + offset);
 }
 String criptografar(String entrada, int chave) {
-  String saida = “”;
+  String saida = "";
   size_t len = entrada.length();
   for (size_t i = 0; i < len; i++)
     saida += converter(entrada[i], chave);
   return saida;
 }
 void postDataToServer() {
-  Serial.println(“Posting JSON data to server...“);
-  String mensagem_cripto = criptografar(“Eu Odeio ESP”, 12);
+  Serial.println("Posting JSON data to server...");
+  String mensagem_cripto = criptografar("AULA CRIPTO", 12);
   // Block until we are able to connect to the WiFi access point
   HTTPClient http;
-    http.begin(“https://ur524n-3000.preview.csb.app/teobaldo”);
-    http.addHeader(“Content-Type”, “application/json”);
+    http.begin("https://ur524n-3000.preview.csb.app/teobaldo");
+    http.addHeader("Content-Type", "application/json");
     StaticJsonDocument<200> doc;
     // Add values in the document
     //
-    doc[“nome”] = “Gabriel Elias Carneiro”;
-    doc[“turma”] = 1;
-    doc[“grupo”] = 1;
-    doc[“mensagem_texto”] = “Eu Odeio ESP”;
-    doc[“mensagem_cripto”] = mensagem_cripto;
+    doc["nome"] = "Rafael Alves Cabral";
+    doc["turma"] = 1;
+    doc["grupo"] = 4;
+    doc["mensagem_texto"] = "RAFAEL CONSEGUIU";
+    doc["mensagem_cripto"] = mensagem_cripto;
      // Add an array.
     //
-    JsonArray data = doc.createNestedArray(“data”);
+    JsonArray data = doc.createNestedArray("data");
     String requestBody;
     serializeJson(doc, requestBody);
     int httpResponseCode = http.POST(requestBody);
